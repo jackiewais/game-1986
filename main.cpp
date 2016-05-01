651ba5757767a55575d3004a01784177d62e1a12
Gnahue <nahuelgalvan@gmail.com>
2016-05-01 14:03:23 -0300

    probando fondo

--- main.cpp
+++ main.cpp
@@ -15,13 +15,23 @@ Escenario* mapa = new Escenario(width,height);
 int x = 200;
 int y = 500;
 //cargo varios backround 
-mapa->insertBackgroundObject("sprites/pelota.png", x,y);
-mapa->insertBackgroundObject("sprites/pelota.png", 500,500);
+mapa->insertBackgroundObject("sprites/spriteJugador.png", x,y);
+mapa->insertBackgroundObject("enfermera", 100,500);
 mapa->insertBackgroundObject("sprites/pelota.png", 200,2000);
 mapa->insertBackgroundObject("sprites/pelota.png", 150,700);
 mapa->insertBackgroundObject("sprites/pelota.png", 100,100);
 mapa->insertBackgroundObject("sprites/pelota.png", 450,657);
-mapa->insertBackgroundObject("sprites/pelota.png", 235,876);
+mapa->insertBackgroundObject("sprites/pelota.png", 300,500);
+
+mapa->insertBackgroundObject("sprites/pelota.png", 100,100);
+mapa->insertBackgroundObject("sprites/pelota.png", 150,100);
+mapa->insertBackgroundObject("sprites/pelota.png", 200,100);
+mapa->insertBackgroundObject("sprites/pelota.png", 250,100);
+mapa->insertBackgroundObject("sprites/pelota.png", 300,100);
+mapa->insertBackgroundObject("sprites/pelota.png", 350,100);
+mapa->insertBackgroundObject("sprites/pelota.png", 400,100);
+mapa->insertBackgroundObject("sprites/pelota.png", 450,100);
+mapa->insertBackgroundObject("sprites/pelota.png", 500,100);
 
 mapa->lunchScreen();
 
